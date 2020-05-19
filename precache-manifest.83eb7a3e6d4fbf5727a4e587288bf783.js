4f1f8956bb828348d97d8a7a353763c86c9ba484
FRSCO <frscollo@hotmail.it>
2020-05-19 14:59:54 +0100

    Updates

---
+++ precache-manifest.83eb7a3e6d4fbf5727a4e587288bf783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9f4c7b7d4e4b2c31983c832cd32615",
+    "url": "/react-animation/index.html"
+  },
+  {
+    "revision": "ae3d4163d51a50aeb4fb",
+    "url": "/react-animation/static/css/main.623e76b0.chunk.css"
+  },
+  {
+    "revision": "33edb6d1e32bb4f92c84",
+    "url": "/react-animation/static/js/2.8b3376a2.chunk.js"
+  },
+  {
+    "revision": "c344ff0e273d3dd0833ad3a0d4c0857e",
+    "url": "/react-animation/static/js/2.8b3376a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3d4163d51a50aeb4fb",
+    "url": "/react-animation/static/js/main.a6526fe2.chunk.js"
+  },
+  {
+    "revision": "1bfa243736239d1b9b5b",
+    "url": "/react-animation/static/js/runtime-main.b027ecac.js"
+  }
+]);
